6b7eba798f02a617b7ebdaf86c154d07076ceacb
herculesPaulWang <you@example.com>
2021-02-15 21:14:58 -0800

    1

--- experiments/train.py
+++ experiments/train.py
@@ -17,7 +17,7 @@ def parse_args():
     parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
     parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
     parser.add_argument("--adv-policy", type=str, default="maddpg", help="policy of adversaries")
-    parser.add_argument("--max_num_train", type=int, default=50, help="number of train")
+    parser.add_argument("--max_num_train", type=int, default=1000, help="number of train")
 
     # Core training parameters
     parser.add_argument("--lr", type=float, default=1e-2, help="learning rate for Adam optimizer")
