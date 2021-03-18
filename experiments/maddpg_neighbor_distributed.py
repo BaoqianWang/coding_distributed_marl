6e6828b9c117d69afd54191588205bdcf589038e
herculesPaulWang <you@example.com>
2021-03-17 23:04:16 -0700

    1

--- experiments/maddpg_neighbor_distributed.py
+++ experiments/maddpg_neighbor_distributed.py
@@ -20,7 +20,7 @@ def parse_args():
     parser.add_argument("--max-episode-len", type=int, default=25, help="maximum episode length")
     parser.add_argument("--num-episodes", type=int, default=20000, help="number of episodes")
     parser.add_argument("--train-period", type=int, default=1000, help="frequency of updating parameters")
-    parser.add_argument("--num_train", type=int, default=100, help="number of train")
+    parser.add_argument("--num_train", type=int, default=200, help="number of train")
     parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
     parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
     parser.add_argument("--adv-policy", type=str, default="maddpg", help="policy of adversaries")
