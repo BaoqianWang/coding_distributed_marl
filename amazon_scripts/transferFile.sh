dfc7d71a3e67fc9e1515f33a39ff1dc36ad04ff7
herculesPaulWang <you@example.com>
2021-03-20 10:17:12 -0700

    1

--- amazon_scripts/transferFile.sh
+++ amazon_scripts/transferFile.sh
@@ -6,7 +6,7 @@ filename='nodeIPaddress'
 while read line; do
 echo $line
 
-scp -i ~/AmazonEC2/.ssh/linux_key_pari.pem $1 ubuntu@$line:~/maddpg/experiments/
+scp -i ~/AmazonEC2/.ssh/linux_key_pari.pem $1 ubuntu@$line:~/maddpg/amazon_scripts/
 
 echo "Transfer $1 to $line Done !"
 done < $filename
