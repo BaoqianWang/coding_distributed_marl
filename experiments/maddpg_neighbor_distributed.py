efa476579f4eac794830d71c9d432b2dca24324a
herculesPaulWang <you@example.com>
2021-03-25 16:41:20 -0700

    1

--- experiments/maddpg_neighbor_distributed.py
+++ experiments/maddpg_neighbor_distributed.py
@@ -20,7 +20,7 @@ def parse_args():
     parser.add_argument("--max-episode-len", type=int, default=25, help="maximum episode length")
     parser.add_argument("--num-episodes", type=int, default=20000, help="number of episodes")
     parser.add_argument("--train-period", type=int, default=1000, help="frequency of updating parameters")
-    parser.add_argument("--num_train", type=int, default=1000, help="number of train")
+    parser.add_argument("--num_train", type=int, default=4000, help="number of train")
     parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
     parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
     parser.add_argument("--adv-policy", type=str, default="maddpg", help="policy of adversaries")
@@ -196,6 +196,9 @@ if __name__== "__main__":
             evaluate_env = make_env(arglist.scenario, arglist, evaluate= True)
             saver = tf.train.Saver()
             final_rewards = []
+            if arglist.restore:
+                print('Loading previous state...')
+                U.load_state(arglist.save_dir)
 
             #interact_with_environments(env, trainers, steps, True)
 
@@ -259,7 +262,7 @@ if __name__== "__main__":
 
                     num_train += 1
                     #print('Num of iteration', num_train)
-                    if(num_train % 20 == 0):
+                    if(num_train % 50 == 0):
                         end_train_time = time.time()
                         U.save_state(arglist.save_dir, saver=saver)
                         rew_evaluate = evaluate_policy(evaluate_env, trainers)
