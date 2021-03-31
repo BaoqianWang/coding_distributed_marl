6c902124c1b2e80deac46b49d8d9bbaf8d416a6c
herculesPaulWang <you@example.com>
2021-03-30 17:32:34 -0700

    1

--- visualize/visualize_rewards.py
+++ visualize/visualize_rewards.py
@@ -4,7 +4,15 @@ import matplotlib.pyplot as plt
 with open('../learning_curves/simple_spreadneighbor_distributed_num_agents_3.pkl','rb') as f:
     data=pickle.load(f)
 
-plt.figure()
-plt.plot(data)
+plt.figure(figsize=(8,5.3))
+plt.plot(data, linewidth=4)
+plt.ylabel('Reward', fontsize=18)
+plt.xlabel('Training iteration (x50)', fontsize=18)
+plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+plt.legend(fontsize=18)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.grid()
+#plt.savefig('3_agents_comparison_without_goal.png', transparent = True)
 plt.show()
 #print(data)
